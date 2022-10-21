7a535fda34d9540cfd032105ba2c695fdb245c4f
naji-alhusami <naji.alhusami@bahcesehir.edu.tr>
2022-10-21 12:47:34 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.getElementById("main");
+main.remove();
+
+const newEl=document.querySelector('body');
+const newHeader = document.createElement('h1')
+
+newHeader.id="victory";
+newHeader.textContent='Naji is the champion';
+
+newEl.append(newHeader);
